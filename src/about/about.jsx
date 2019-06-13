47970942d405365d68e01b5bc4dec8c448d3ea08
unknown <lrozalem@gmail.com>
2019-06-13 09:13:15 -0300

    first commit

---
+++ src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nos'></PageHeader>
+        <h2>Nossa Historia</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Missão e visão</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolot sit amet...</p>
+    </div>
+)
